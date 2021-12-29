4210465c595f5d63ab62e3e94be41f73779f34e6
Sidenko M. E <matvey.sidenko@mail.ru>
2021-12-29 12:24:43 +0700

    First commit for C#

---
+++ Prog_lab10_csharp/Human.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Globalization;
+
+namespace Prog_lab6
+{
+    public class Human
+    {
+		//Attributes
+		private int id;
+		private int age;
+		private int height;
+		private double weight;
+		private char gender;
+		public Fio fioField;
+
+		//Properties
+		public int Id
+		{
+			get
+			{
+				return id;
+			}
+			set
+			{
+				if ((value >= 0) && (value <= 9999))
+					id = value;
+			}
+		}
+		public int Age 
+		{
+			get
+			{
+				return age;
+			}
+			set
+			{
+				if ((value >= 0) && (value <= 200))
+					age = value;
+			}
+		}
+		public int Height
+		{
+			get
+			{
+				return height;
+			}
+			set
+			{
+				if ((value >= 40) && (value <= 300))
+					height = value;
+			}
+		}
+		public double Weight
+		{
+			get
+			{
+				return weight;
+			}
+			set
+			{
+				value = Math.Round(value, 1, MidpointRounding.AwayFromZero);
+				if ((value >= 0) && (value <= 650))
+					weight = value;
+			}
+		}
+		public char Gender
+        {
+			get
+			{
+				return gender;
+			}
+			set
+			{
+				if (value == 'M' || value == 'F')
+					gender = value;
+			}
+		}
+		public Fio FioField 
+		{
+			get
+			{
+				return (fioField);
+			}
+			set
+			{
+				fioField = value;
+			}
+		}
+
+		//Constructors
+		public Human()
+		{
+			id = 0;
+			age = 0;
+			height = 40;
+			weight = 0;
+			gender = 'M';
+			fioField = new Fio();
+		}
+		public Human(int bufId)
+		{
+			id = 0;
+			age = 0;
+			height = 40;
+			weight = 0;
+			gender = 'M';
+			fioField = new Fio();
+
+			SetId(bufId);
+		}
+		public Human(int bufId, int bufAge, int bufHeight, double bufWeight, char bufGender, Fio bufFio)
+		{
+			Human check = new Human();
+
+			if (check.SetId(bufId) || check.SetAge(bufAge) || check.SetHeight(bufHeight) ||
+				check.SetWeight(bufWeight) || check.SetGender(bufGender))
+			{
+				id = 0;
+				age = 0;
+				height = 40;
+				weight = 0;
+				gender = 'M';
+				fioField = new Fio();
+			}
+			else
+			{
+				SetId(bufId);
+				SetAge(bufAge);
+				SetHeight(bufHeight);
+				SetWeight(bufWeight);
+				SetGender(bufGender);
+				fioField = new Fio(bufFio.GetSurname(), bufFio.GetName(), bufFio.GetPatronymic());
+			}
+		}
+
+		//Methods
+		public bool SetId(int buf)
+		{
+			if (buf < 0 || buf > 9999)
+				return (true);
+			else
+			{
+				id = buf;
+				return (false);
+			}
+		}
+		public bool SetAge(int buf)
+		{
+			if (buf < 0 || buf > 200)
+				return (true);
+			else
+			{
+				age = buf;
+				return (false);
+			}
+		}
+		public bool SetHeight(int buf)
+		{
+			if (buf < 40 || buf > 300)
+				return (true);
+			else
+			{
+				height = buf;
+				return (false);
+			}
+		}
+		public bool SetWeight(double buf)
+		{
+			//round *.* format to *.1 format 
+			buf = Math.Round(buf, 1, MidpointRounding.AwayFromZero);
+			if (buf < 0 || buf > 650)
+				return (true);
+			else
+			{
+				weight = buf;
+				return (false);
+			}
+		}
+		public bool SetGender(char buf)
+		{
+			if (buf == 'M' || buf == 'F')
+			{
+				gender = buf;
+				return (false);
+			}
+			else
+				return (true);
+		}
+		public bool SetAll(int bufId, int bufAge, int bufHeight, double bufWeight, char bufGender, Fio bufFio)
+		{
+			Human check = new Human();
+
+			if (check.SetId(bufId) || check.SetAge(bufAge) || check.SetHeight(bufHeight) ||
+				check.SetWeight(bufWeight) || check.SetGender(bufGender))
+				return (true);
+			else
+			{
+				SetId(check.GetId());
+				SetAge(check.GetAge());
+				SetHeight(check.GetHeight());
+				SetWeight(check.GetWeight());
+				SetGender(check.GetGender());
+				fioField.SetAll(bufFio.GetSurname(), bufFio.GetName(), bufFio.GetPatronymic());
+				return (false);
+			}
+		}
+
+		public int GetId()
+		{ return (id); }
+		public int GetAge()
+		{ return (age); }
+		public int GetHeight()
+		{ return (height); }
+		public double GetWeight()
+		{ return (weight); }
+		public char GetGender()
+		{ return (gender); }
+
+		public bool Read()
+		{
+			Human check = new Human();
+			int bufInt;
+			double bufDouble;
+			char bufChar;
+
+			Console.Write("Enter id:\n");
+			if (!int.TryParse(Console.ReadLine(), out bufInt))
+				return (true);
+			if (check.SetId(bufInt))
+				return (true);
+
+
+			Console.Write("Enter age:\n");
+			if (!int.TryParse(Console.ReadLine(), out bufInt))
+				return (true);
+			if (check.SetAge(bufInt))
+				return (true);
+
+
+			Console.Write("Enter height:\n");
+			if (!int.TryParse(Console.ReadLine(), out bufInt))
+				return (true);
+			if (check.SetHeight(bufInt))
+				return (true);
+
+
+			Console.Write("Enter weight:\n");
+			if (!double.TryParse(Console.ReadLine(), NumberStyles.AllowDecimalPoint, CultureInfo.CreateSpecificCulture("fr-FR"), out bufDouble))
+				return (true);
+			if (check.SetWeight(bufDouble))
+				return (true);
+
+
+			Console.Write("Enter gender:\n");
+			if (!char.TryParse(Console.ReadLine(), out bufChar))
+				return (true);
+			if (check.SetGender(bufChar))
+				return (true);
+
+
+			if (check.fioField.Read())
+				return (true);
+
+
+			SetAll(check.GetId(), check.GetAge(), 
+				check.GetHeight(), check.GetWeight(), 
+				check.GetGender(), check.fioField);
+			return (false);
+		}
+		public void Display()
+		{
+			Console.Write("id: {0}\n", id);
+			Console.Write("age: {0}\n", age);
+			Console.Write("height: {0}\n", height);
+			Console.Write("weight: {0:f1}\n", weight);
+			Console.Write("gender: {0}\n", gender);
+			fioField.Display();
+		}
+	}
+}
---
+++ Prog_lab10_csharp/Student.cs
@@ -0,0 +1,305 @@
+﻿using System;
+
+namespace Prog_lab6
+{
+	public class Student
+	{
+		//Attributes
+		private int course;
+		private string eduProg;
+		private string group;
+		private string facultyName;
+		public Human humanField;
+
+		//Properties
+		public int Course 
+		{
+			get
+			{
+				return course;
+			}
+			set
+			{
+				if ((value >= 0) && (value <= 6))
+					course = value;
+			}
+		}
+		public string EduProg 
+		{
+			get
+			{
+				string outputString = new string(eduProg.ToCharArray());
+				return (outputString);
+			}
+			set
+			{
+				if (!string.IsNullOrEmpty(value))
+				{
+					string invalidSymbStr = "!@#$%^&*()_+1234567890-=\";:?*,./'][{}<>~` ";
+					char[] invalidSymbols = invalidSymbStr.ToCharArray();
+
+					bool checkFlag = false;
+
+					foreach (char symb in invalidSymbols)
+					{
+						if (value.IndexOf(symb) != (-1))
+							checkFlag = true;
+					}
+
+					if (!checkFlag)
+						eduProg = new string(value.ToCharArray());
+				}
+			}
+		}
+		public string Group 
+		{
+			get
+			{
+				string outputString = new string(group.ToCharArray());
+				return (outputString);
+			}
+			set
+			{
+				if (!string.IsNullOrEmpty(value))
+				{
+					string invalidSymbStr = "!@#$%^&*()_+=\";:?*,./'][{}<>~` ";
+					char[] invalidSymbols = invalidSymbStr.ToCharArray();
+
+					bool checkFlag = false;
+
+					foreach (char symb in invalidSymbols)
+					{
+						if (value.IndexOf(symb) != (-1))
+							checkFlag = true;
+					}
+
+					if (!checkFlag)
+						group = new string(value.ToCharArray());
+				}
+			}
+		}
+		public string FacultyName 
+		{
+			get
+			{
+				string outputString = new string(facultyName.ToCharArray());
+				return (outputString);
+			}
+			set
+			{
+				if (!string.IsNullOrEmpty(value))
+				{
+					string invalidSymbStr = "!@#$%^&*()_+1234567890-=\";:?*,./'][{}<>~` ";
+					char[] invalidSymbols = invalidSymbStr.ToCharArray();
+
+					bool checkFlag = false;
+
+					foreach (char symb in invalidSymbols)
+					{
+						if (value.IndexOf(symb) != (-1))
+							checkFlag = true;
+					}
+
+					if (!checkFlag)
+						facultyName = new string(value.ToCharArray());
+				}
+			}
+		}
+		public Human HumanField 
+		{
+			get
+			{
+				return (humanField);
+			}
+			set
+			{
+				humanField = value;
+			}
+		}
+
+		//Constructors
+		public Student()
+		{
+			course = 0;
+			eduProg = "unstated";
+			group = "unstated";
+			facultyName = "unstated";
+			humanField = new Human();
+		}
+		public Student(Human bufHuman)
+		{
+			course = 0;
+			eduProg = "unstated";
+			group = "unstated";
+			facultyName = "unstated";
+			humanField = new Human(bufHuman.GetId(), bufHuman.GetAge(), 
+				bufHuman.GetHeight(), bufHuman.GetWeight(), 
+				bufHuman.GetGender(), bufHuman.fioField);
+		}
+		public Student(int bufCourse, string bufEduProg, string bufGroup, string bufFacultyName, Human bufHuman)
+		{
+			Student check = new Student();
+
+			if (check.SetCourse(bufCourse) || check.SetEduProg(bufEduProg) || check.SetGroup(bufGroup) ||
+				check.SetFacultyName(bufFacultyName))
+			{
+				course = 0;
+				eduProg = "unstated";
+				group = "unstated";
+				facultyName = "unstated";
+				humanField = new Human();
+			}
+			else
+			{
+				SetCourse(bufCourse);
+				SetEduProg(bufEduProg);
+				SetGroup(bufGroup);
+				SetFacultyName(bufFacultyName);
+				humanField = new Human(bufHuman.GetId(), bufHuman.GetAge(),
+				bufHuman.GetHeight(), bufHuman.GetWeight(), 
+				bufHuman.GetGender(), bufHuman.fioField);
+			}
+		}
+
+		//Methods
+		public bool SetCourse(int buf)
+		{
+			if (buf < 0 || buf > 6)
+				return (true);
+			else
+			{
+				course = buf;
+				return (false);
+			}
+		}
+		public bool SetEduProg(string bufString)
+		{
+			if (string.IsNullOrEmpty(bufString))
+				return (true);
+
+			string invalidSymbStr = "!@#$%^&*()_+1234567890-=\";:?*,./'][{}<>~` ";
+			char[] invalidSymbols = invalidSymbStr.ToCharArray();
+			foreach (char symb in invalidSymbols)
+			{
+				if (bufString.IndexOf(symb) != (-1))
+					return (true);
+			}
+
+			eduProg = new string (bufString.ToCharArray());
+			return (false);
+		}
+		public bool SetGroup(string bufString)
+		{
+			if (string.IsNullOrEmpty(bufString))
+				return (true);
+
+			string invalidSymbStr = "!@#$%^&*()_+=\";:?*,./'][{}<>~` ";
+			char[] invalidSymbols = invalidSymbStr.ToCharArray();
+			foreach (char symb in invalidSymbols)
+			{
+				if (bufString.IndexOf(symb) != (-1))
+					return (true);
+			}
+
+			group = new string (bufString.ToCharArray());
+			return (false);
+		}
+		public bool SetFacultyName(string bufString)
+		{
+			if (string.IsNullOrEmpty(bufString))
+				return (true);
+
+			string invalidSymbStr = "!@#$%^&*()_+1234567890-=\";:?*,./'][{}<>~` ";
+			char[] invalidSymbols = invalidSymbStr.ToCharArray();
+			foreach (char symb in invalidSymbols)
+			{
+				if (bufString.IndexOf(symb) != (-1))
+					return (true);
+			}
+
+			facultyName = new string (bufString.ToCharArray());
+			return (false);
+		}
+		public bool SetAll(int bufCourse, string bufEduProg, string bufGroup, string bufFacultyName, Human bufHuman)
+		{
+			Student check = new Student();
+
+			if (check.SetCourse(bufCourse) || check.SetEduProg(bufEduProg) || check.SetGroup(bufGroup) ||
+				check.SetFacultyName(bufFacultyName))
+				return (true);
+			else
+			{
+				SetCourse(bufCourse);
+				SetEduProg(bufEduProg);
+				SetGroup(bufGroup);
+				SetFacultyName(bufFacultyName);
+				humanField.SetAll(bufHuman.GetId(), bufHuman.GetAge(),
+				bufHuman.GetHeight(), bufHuman.GetWeight(), bufHuman.GetGender(), bufHuman.fioField);
+				return (false);
+			}
+		}
+
+		public int GetCourse()
+		{ return (course); }
+		public string GetEduProg()
+		{
+			string outputString = new string(eduProg.ToCharArray());
+			return (outputString);
+		}
+		public string GetGroup()
+		{
+			string outputString = new string (group.ToCharArray());
+			return (outputString);
+		}
+		public string GetFacultyName()
+		{
+			string outputString = new string (facultyName.ToCharArray());
+			return (outputString);
+		}
+
+		public bool Read()
+		{
+			Student check = new Student();
+			int bufInt;
+
+			Console.Write("Enter education programm:\n");
+			if (check.SetEduProg(Console.ReadLine()))
+				return (true);
+
+
+			Console.Write("Enter group:\n");
+			if (check.SetGroup(Console.ReadLine()))
+				return (true);
+
+
+			Console.Write("Enter faculty name:\n");
+			if (check.SetFacultyName(Console.ReadLine()))
+				return (true);
+
+
+			Console.Write("Enter course:\n");
+			if (!int.TryParse(Console.ReadLine(), out bufInt))
+				return (true);
+			if (check.SetCourse(bufInt))
+				return (true);
+
+
+			if (check.humanField.Read())
+				return (true);
+			
+
+			SetAll(check.GetCourse(), check.GetEduProg(), check.GetGroup(),
+			check.GetFacultyName(), check.humanField);
+			return (false);
+		}
+		public void Display()
+		{
+			Console.Write("course: {0}\n", course);
+			Console.Write("direction of preparation: {0}\n", eduProg);
+			Console.Write("group: {0}\n", group);
+			Console.Write("faculty name: {0}\n", facultyName);
+			humanField.Display();
+		}
+    }
+}
+
